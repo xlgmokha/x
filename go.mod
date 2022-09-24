38a0f557ffa30ead383ca5b2d78baea4d500a333
mo khan <mo@mokhan.ca>
2022-09-23 19:32:30 -0600

    chore: re-organize directory structure

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/xlg-pkg/x
+module github.com/xlgmokha/x
 
-go 1.16
+go 1.18
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/xlg-pkg/x/stdout"
-
-func main() {
-	stdout.Puts("You lost?")
-}
